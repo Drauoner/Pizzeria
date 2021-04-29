5b3d556bb07e8288dde20aada00e1849a135e8cc
Alex <alex_renouard1@naylor.edu>
2021-04-29 14:34:22 -0500

    commit_1

--- pizzas/views.py
+++ pizzas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def homepage(request):
+    """Home page for the Pizzeria"""
+    return render(request, "pizzas/homepage.html")

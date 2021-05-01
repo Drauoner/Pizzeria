bf036f87274cf71ac47df0f01c72d4decc544504
Alex <alex_renouard1@naylor.edu>
2021-05-01 01:18:27 -0500

    commit_4_entry_set_bug

--- pizzas/tests.py
+++ pizzas/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+
+
+#pizza.html
+#<p> <a href="{% url 'pizzas:new_comment' pizza.id %}">Add new a comment</a></p>
--- pizzas/views.py
+++ pizzas/views.py
@@ -16,8 +16,29 @@ def menu(request):
 
 def pizza(request, pizza_id):
     pizza = Pizza.objects.get(id=pizza_id)
-    context = {"pizza":pizza}
+    #toppings = pizza.entry_set.order_by('-date_added')
+    context = {"pizza":pizza, "toppings":toppings}
     return render(request, "pizzas/pizza.html", context)
+'''
+def new_comment(request, pizza_id):
+    pizza = Pizza.objects.get(id=pizza_id)
+    context = {"pizza": pizza}
+    return render(request, "pizzas/new_comment.html", context)
+''' 
+'''
+    if request.method != "POST":
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect("MainApp:topic", topic_id=topic_id)
+
+    context = {"form": form, "pizza": pizza}
+'''
+
 
 '''
 def topping(request, pizza_id):

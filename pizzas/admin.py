7643a6d5be05272e92a86c8e55745caa730be627
Alex <alex_renouard1@naylor.edu>
2021-05-01 14:45:06 -0500

    commit_final

--- pizzas/admin.py
+++ pizzas/admin.py
@@ -6,3 +6,12 @@ from .models import Pizza, Topping, Comment
 admin.site.register(Pizza)
 admin.site.register(Topping)
 admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'pizza', 'date_added', 'active')
+    list_filter = ('active', 'date_added')
+    search_fields = ('name', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
